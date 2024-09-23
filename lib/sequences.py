a701d509f4d4f121d7124e3843ebbfa17b7c4cd8
R-y-a-n8 <ryan.kinuthia@student.moringaschool.com>
2024-09-24 00:07:58 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    fibonacci_sequence = [0, 1]  
+    if length <= 0:
+        return []  
+    elif length == 1:
+        return [0] 
+    elif length == 2:
+        return [0, 1]  
+    
+    fibonacci_sequence = [0, 1] 
     while len(fibonacci_sequence) < length:
-        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]  
-        fibonacci_sequence.append(next_number)  
-    print(fibonacci_sequence[:length])  
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]  
+        fibonacci_sequence.append(next_value)  
+    return fibonacci_sequence
 
